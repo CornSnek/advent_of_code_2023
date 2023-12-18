361e246369468fe8ae7f7840a118ef5486893060
Jeremy Daffon <jdaffon123@gmail.com>
2023-12-18 11:09:40 -0800

    Finished day 13 part 2

--- src/day13/main.zig
+++ src/day13/main.zig
@@ -4,12 +4,10 @@ const IntT = u64;
 var input_file: []const u8 = undefined;
 pub fn main() !void {
     input_file = @import("root").input_file;
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer if (gpa.deinit() == .leak) std.debug.print("Leak detected.\n", .{});
-    const p = try do_puzzle(gpa.allocator());
+    const p = do_puzzle();
     std.debug.print("{}\n", .{p});
-    try std.testing.expectEqual(@as(IntT, 33780), p.p1);
-    //try std.testing.expectEqual(@as(IntT, 550358864332), p.p2);
+    //try std.testing.expectEqual(@as(IntT, 33780), p.p1);
+    //try std.testing.expectEqual(@as(IntT, 23479), p.p2);
 }
 pub fn parse_line(file_str: []const u8, pos: *usize) ?[]const u8 {
     if (pos.* == file_str.len) return null;
@@ -45,45 +43,72 @@ inline fn get_elem(comptime bounds_check: bool, comptime ArrayT: anytype, array_
     if (bounds_check) if (vec.x < 0 or vec.y < 0 or vec.x >= @as(isize, @intCast(TextWidth - 1)) or vec.y >= @as(isize, @intCast(TextHeight))) return null;
     return array_t[TextWidth * @as(usize, @intCast(vec.y)) + @as(usize, @intCast(vec.x))];
 }
-pub fn do_puzzle(allocator: std.mem.Allocator) !struct { p1: IntT, p2: IntT } {
-    _ = allocator;
+pub fn do_puzzle() struct { p1: IntT, p2: IntT } {
     var pos: usize = 0;
     var steps_p1: IntT = 0;
     var steps_p2: IntT = 0;
     while (parse_map(input_file, &pos)) |map| {
-        var matching_col: ?usize = next_col: for (0..map.width - 2) |col| {
+        if (calculate(.part1, .column, map)) |matching_col| steps_p1 += matching_col;
+        if (calculate(.part1, .row, map)) |matching_row| steps_p1 += matching_row * 100;
+        if (calculate(.part2, .column, map)) |matching_col| steps_p2 += matching_col;
+        if (calculate(.part2, .row, map)) |matching_row| steps_p2 += matching_row * 100;
+    }
+    return .{ .p1 = steps_p1, .p2 = steps_p2 };
+}
+pub const PuzzlePart = enum { part1, part2 };
+pub const ReflectionType = enum { row, column };
+pub fn calculate(comptime pp: PuzzlePart, comptime rt: ReflectionType, map: Map) ?usize {
+    if (rt == .column) {
+        next_col: for (0..map.width - 2) |col| {
             var col1: isize = @intCast(col);
             var col2: isize = @intCast(col + 1);
+            var exactly_one_mistake = if (pp == .part2) @as(bool, false) else @as(void, {});
             while (true) : ({
                 col1 -= 1;
                 col2 += 1;
             }) {
                 var check_row: isize = 0;
                 while (check_row < map.height) : (check_row += 1) {
-                    var ch1 = get_elem(true, @TypeOf(map.map), map.map, @intCast(map.width), @intCast(map.height), .{ .x = col1, .y = check_row }) orelse break :next_col col + 1;
-                    var ch2 = get_elem(true, @TypeOf(map.map), map.map, @intCast(map.width), @intCast(map.height), .{ .x = col2, .y = check_row }) orelse break :next_col col + 1;
-                    if (ch1 != ch2) continue :next_col;
+                    var ch1 = get_elem(true, @TypeOf(map.map), map.map, @intCast(map.width), @intCast(map.height), .{ .x = col1, .y = check_row }) orelse {
+                        if (pp == .part1 or exactly_one_mistake) return col + 1;
+                        continue :next_col;
+                    };
+                    var ch2 = get_elem(true, @TypeOf(map.map), map.map, @intCast(map.width), @intCast(map.height), .{ .x = col2, .y = check_row }) orelse {
+                        if (pp == .part1 or exactly_one_mistake) return col + 1;
+                        continue :next_col;
+                    };
+                    if (ch1 != ch2) {
+                        if (pp == .part1 or exactly_one_mistake) continue :next_col;
+                        exactly_one_mistake = true;
+                    }
                 }
             }
-        } else null;
-        var matching_row: ?usize = next_row: for (0..map.height - 1) |row| {
+        } else return null;
+    } else {
+        next_row: for (0..map.height - 1) |row| {
             var row1: isize = @intCast(row);
             var row2: isize = @intCast(row + 1);
+            var exactly_one_mistake = if (pp == .part2) @as(bool, false) else @as(void, {});
             while (true) : ({
                 row1 -= 1;
                 row2 += 1;
             }) {
                 var check_col: isize = 0;
                 while (check_col < map.width - 1) : (check_col += 1) {
-                    var ch1 = get_elem(true, @TypeOf(map.map), map.map, @intCast(map.width), @intCast(map.height), .{ .x = check_col, .y = row1 }) orelse break :next_row row + 1;
-                    var ch2 = get_elem(true, @TypeOf(map.map), map.map, @intCast(map.width), @intCast(map.height), .{ .x = check_col, .y = row2 }) orelse break :next_row row + 1;
-                    if (ch1 != ch2) continue :next_row;
+                    var ch1 = get_elem(true, @TypeOf(map.map), map.map, @intCast(map.width), @intCast(map.height), .{ .x = check_col, .y = row1 }) orelse {
+                        if (pp == .part1 or exactly_one_mistake) return row + 1;
+                        continue :next_row;
+                    };
+                    var ch2 = get_elem(true, @TypeOf(map.map), map.map, @intCast(map.width), @intCast(map.height), .{ .x = check_col, .y = row2 }) orelse {
+                        if (pp == .part1 or exactly_one_mistake) return row + 1;
+                        continue :next_row;
+                    };
+                    if (ch1 != ch2) {
+                        if (pp == .part1 or exactly_one_mistake) continue :next_row;
+                        exactly_one_mistake = true;
+                    }
                 }
             }
-        } else null;
-        std.debug.assert((matching_col == null and matching_row != null) or (matching_col != null and matching_row == null)); //Either row/column but not both or neither.
-        steps_p1 += if (matching_col != null) matching_col.? else 0 + if (matching_row != null) matching_row.? * 100 else 0;
-        std.debug.print("matching left: {?} down: {?}\n", .{ matching_col, matching_row });
+        } else return null;
     }
-    return .{ .p1 = steps_p1, .p2 = steps_p2 };
 }
