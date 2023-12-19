40f40a89b8ce4a8b07c9af4d0bd29f50136b68da
Jeremy Daffon <jdaffon123@gmail.com>
2023-12-19 11:31:01 -0800

    Day 19 finished part 2

--- src/day19/main.zig
+++ src/day19/main.zig
@@ -65,50 +65,110 @@ pub fn do_puzzle(allocator: std.mem.Allocator) !struct { p1: IntT, p2: IntT } {
         }
     }
     var p1: IntT = 0;
+    var p2: IntT = 0;
     while (parse_line(input_file, &pos)) |line| {
         var line_pos: usize = 0;
-        const var_arr_p1 = VariableArr{
-            (try parse_number(line, &line_pos)) orelse return error.ExpectedNumber,
-            (try parse_number(line, &line_pos)) orelse return error.ExpectedNumber,
-            (try parse_number(line, &line_pos)) orelse return error.ExpectedNumber,
-            (try parse_number(line, &line_pos)) orelse return error.ExpectedNumber,
-        };
-        try parse_workflow(&workflow_map, &p1, var_arr_p1);
+        const xv = (try parse_number(line, &line_pos)) orelse return error.ExpectedNumber;
+        const mv = (try parse_number(line, &line_pos)) orelse return error.ExpectedNumber;
+        const av = (try parse_number(line, &line_pos)) orelse return error.ExpectedNumber;
+        const sv = (try parse_number(line, &line_pos)) orelse return error.ExpectedNumber;
+        const range_p1 = [4]RatingRange{ .{ .min = xv, .max = xv }, .{ .min = mv, .max = mv }, .{ .min = av, .max = av }, .{ .min = sv, .max = sv } };
+        const wf_result_p1 = try parse_workflow(allocator, workflow_map, range_p1);
+        defer allocator.free(wf_result_p1);
+        if (wf_result_p1.len != 0) p1 += wf_result_p1[0].rr[0].min + wf_result_p1[0].rr[1].min + wf_result_p1[0].rr[2].min + wf_result_p1[0].rr[3].min;
     }
-    return .{ .p1 = p1, .p2 = 0 };
+    const wf_result = try parse_workflow(allocator, workflow_map, .{ .{ .min = 1, .max = 4000 }, .{ .min = 1, .max = 4000 }, .{ .min = 1, .max = 4000 }, .{ .min = 1, .max = 4000 } });
+    defer allocator.free(wf_result);
+    for (wf_result) |rrs| {
+        var mult: IntT = 1;
+        for (rrs.rr) |range| mult *= range.max - range.min + 1;
+        p2 += mult;
+    }
+    return .{ .p1 = p1, .p2 = p2 };
 }
-pub fn parse_workflow(workflow_map: *WorkflowMap, num_answer: *IntT, var_arr: VariableArr) !void {
-    var current_wf: WorkflowName = .{ 'i', 'n', 0 };
-    done: while (true) {
-        var wf_arr: []Workflow = workflow_map.get(current_wf) orelse return error.ExpectedWorkflow;
-        goto_wf: for (wf_arr) |wf| {
-            switch (wf) {
-                .accept => {
-                    num_answer.* += var_arr[0] + var_arr[1] + var_arr[2] + var_arr[3];
-                    break :done;
-                },
-                .reject => break :done,
-                .instructions => |ins| {
-                    if (ins.s == .gt) {
-                        if (var_arr[@intFromEnum(ins.t)] > ins.n) {
-                            current_wf = ins.g;
-                            break :goto_wf;
+const RatingRangeSplits = struct { rr: [4]RatingRange };
+pub fn parse_workflow(allocator: std.mem.Allocator, workflow_map: WorkflowMap, rating_range: [4]RatingRange) ![]RatingRangeSplits { //Returns accepted ranges.
+    var range_splits = try allocator.alloc(RatingRangeSplits, 1);
+    errdefer allocator.free(range_splits);
+    range_splits[0] = .{ .rr = rating_range };
+    var wf_states = try allocator.alloc(WorkflowState, 1); //To copy ranges' state when split.
+    defer allocator.free(wf_states);
+    wf_states[0] = .{ .wn = .{ 'i', 'n', 0 }, .wf_i = 0 };
+    var chunk_len: usize = 1;
+    var chunk_i: usize = 0;
+    next_chunk: while (chunk_i < chunk_len) {
+        while (true) {
+            const wf_arr: []Workflow = workflow_map.get(wf_states[chunk_i].wn) orelse return error.ExpectedDefinedWorkflow;
+            goto_next_wf: while (wf_states[chunk_i].wf_i < wf_arr.len) : (wf_states[chunk_i].wf_i += 1) {
+                switch (wf_arr[wf_states[chunk_i].wf_i]) {
+                    .accept => {
+                        chunk_i += 1;
+                        continue :next_chunk;
+                    },
+                    .reject => {
+                        chunk_len -= 1; //Overwrite the rejected with the last in the array.
+                        range_splits[chunk_i] = range_splits[chunk_len];
+                        wf_states[chunk_i] = wf_states[chunk_len];
+                        continue :next_chunk;
+                    },
+                    .instructions => |ins| {
+                        const accept_rr = if (ins.s == .gt) RatingRange{ .min = ins.n + 1, .max = std.math.maxInt(IntT) } else RatingRange{ .min = 0, .max = ins.n - 1 };
+                        const splits = range_splits[chunk_i].rr[@intFromEnum(ins.t)].splits(accept_rr);
+                        if (splits != null) {
+                            if (splits.?.l) |sp_l| {
+                                var chunks_rej = range_splits[chunk_i].rr;
+                                chunks_rej[@intFromEnum(ins.t)] = sp_l;
+                                if (chunk_len == range_splits.len) {
+                                    range_splits = try allocator.realloc(range_splits, 2 * range_splits.len);
+                                    wf_states = try allocator.realloc(wf_states, 2 * wf_states.len);
+                                }
+                                range_splits[chunk_len].rr = chunks_rej;
+                                wf_states[chunk_len] = wf_states[chunk_i];
+                                wf_states[chunk_len].wf_i += 1;
+                                chunk_len += 1;
+                            }
+                            if (splits.?.u) |sp_u| {
+                                var chunks_rej = range_splits[chunk_i].rr;
+                                chunks_rej[@intFromEnum(ins.t)] = sp_u;
+                                if (chunk_len == range_splits.len) {
+                                    range_splits = try allocator.realloc(range_splits, 2 * range_splits.len);
+                                    wf_states = try allocator.realloc(wf_states, 2 * wf_states.len);
+                                }
+                                range_splits[chunk_len].rr = chunks_rej;
+                                wf_states[chunk_len] = wf_states[chunk_i];
+                                wf_states[chunk_len].wf_i += 1;
+                                chunk_len += 1;
+                            }
+                            range_splits[chunk_i].rr[@intFromEnum(ins.t)] = splits.?.i;
+                            wf_states[chunk_i] = .{ .wn = ins.g };
+                            break :goto_next_wf;
                         }
-                    } else {
-                        if (var_arr[@intFromEnum(ins.t)] < ins.n) {
-                            current_wf = ins.g;
-                            break :goto_wf;
-                        }
-                    }
-                },
-                .goto => |goto_wf| {
-                    current_wf = goto_wf;
-                    break :goto_wf;
-                },
+                    },
+                    .goto => |goto_next_wf| {
+                        wf_states[chunk_i] = .{ .wn = goto_next_wf };
+                        break :goto_next_wf;
+                    },
+                }
             }
         }
     }
+    range_splits = try allocator.realloc(range_splits, chunk_len);
+    return range_splits;
 }
+const RatingRange = struct {
+    min: IntT,
+    max: IntT,
+    const ThreeSplit = struct { i: RatingRange, l: ?RatingRange, u: ?RatingRange };
+    //Splits range into any intersections and any lower/upper boundries if they exist.
+    pub fn splits(this_rr: RatingRange, split_rr: RatingRange) ?ThreeSplit {
+        const intersect = RatingRange{ .min = @max(split_rr.min, this_rr.min), .max = @min(split_rr.max, this_rr.max) };
+        return if (intersect.min <= intersect.max) .{
+            .i = intersect,
+            .l = if (intersect.min >= this_rr.min and intersect.min != 0 and this_rr.min <= intersect.min - 1) .{ .min = this_rr.min, .max = intersect.min - 1 } else null,
+            .u = if (intersect.max <= this_rr.max and intersect.max != std.math.maxInt(IntT) and this_rr.max >= intersect.max + 1) .{ .min = intersect.max + 1, .max = this_rr.max } else null,
+        } else null;
+    }
+};
 pub fn slice_as_wn(line: []const u8, line_pos: *usize) !WorkflowName {
     const wn_slice = parse_letters(line, line_pos) orelse return error.ExpectedName;
     std.debug.assert(wn_slice.len <= 3);
@@ -118,13 +178,13 @@ pub fn slice_as_wn(line: []const u8, line_pos: *usize) !WorkflowName {
 }
 var input_file: []const u8 = undefined;
 const Variable = enum { x, m, a, s, none };
-const VariableArr = [4]IntT; //x,m,a,s only
 const ChToToken = v: { // zig fmt: off
     var arr = [1]Variable{.none} ** 256; arr['x'] = .x; arr['m'] = .m; arr['a'] = .a; arr['s'] = .s; break :v arr;
 }; // zig fmt: on
 const Sign = enum { gt, lt };
 const Instructions = struct { t: Variable, s: Sign, n: IntT, g: WorkflowName };
 const Workflow = union(enum) { accept: void, reject: void, instructions: Instructions, goto: WorkflowName };
+const WorkflowState = struct { wn: WorkflowName, wf_i: usize = 0 };
 const WorkflowName = [3]u8;
 const WorkflowMap = std.AutoHashMapUnmanaged([3]u8, []Workflow);
 pub fn main() !void {
@@ -133,6 +193,6 @@ pub fn main() !void {
     defer if (gpa.deinit() == .leak) std.debug.print("Leak detected.\n", .{});
     const p = try do_puzzle(gpa.allocator());
     std.debug.print("{}\n", .{p});
-    //try std.testing.expectEqual(@as(IntT, 0), p.p1);
-    //try std.testing.expectEqual(@as(IntT, 0), p.p2);
+    //try std.testing.expectEqual(@as(IntT, 4380), p.p1);
+    //try std.testing.expectEqual(@as(IntT, 116606738659695), p.p2);
 }
